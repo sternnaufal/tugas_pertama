3292e4ba87d9df0978b2deb0bbe64f00bc998ba1
Naufal Rakha Putra <naufalrakha2712@gmail.com>
2026-08-30 12:18:36 +0700

    refactor: move main() to main.dart for flutter run output

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'user_model.dart';
 
 void main() {
+  Map<String, dynamic> jsonResponse = {
+    'name': 'Budi Santoso',
+    'age': 22,
+  };
+
+  UserModel user = UserModel.fromJson(jsonResponse);
+
+  print('Nama: ${user.name}');
+  print('ID: ${user.id}');
+  print('Status: ${user.isActive}');
+  print('To JSON: ${user.toJson()}');
+
   runApp(const MyApp());
 }
 
--- lib/user_model.dart
+++ lib/user_model.dart
@@ -33,17 +33,3 @@ class UserModel {
     };
   }
 }
-
-void main() {
-  Map<String, dynamic> jsonResponse = {
-    'name': 'Budi Santoso',
-    'age': 22,
-  };
-
-  UserModel user = UserModel.fromJson(jsonResponse);
-
-  print('Nama: ${user.name}');
-  print('ID: ${user.id}');
-  print('Status: ${user.isActive}');
-  print('To JSON: ${user.toJson()}');
-}
